1a8b3b6f25f9300e3c5789e71aaba76b7f01aa07
kennarddh <kennarddh@gmail.com>
2022-04-27 01:19:30 +0700

    docs: Add link to codewars CountingDuplicates

--- kata/CountingDuplicates.js
+++ kata/CountingDuplicates.js
@@ -1,3 +1,6 @@
+// Counting Duplicates
+// https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1
+
 function duplicateCount(text) {
 	const allLetters = [...new Set(text.toLowerCase().split(''))]
 
