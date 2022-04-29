f0f738b7fd97ea95b8586a007462fbb2d6c972b1
kennarddh <kennarddh@gmail.com>
2022-04-29 10:14:08 +0700

    chore: Fix new.sh

--- new.sh
+++ new.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # $1 = Name
 # $2 = Url
 # $3 = Number of test cases
@@ -7,33 +9,35 @@ echo 'Creating new kata'
 
 echo 'name: '$1''
 echo 'url: '$2''
+echo 'number of test cases: '$3''
+echo 'pascal case name: '$4''
 
 # Create new kata
 
-echo '// '$1'' >> ./kata/$1.js
-echo '// '$2'' >> ./kata/$1.js
-echo '' >> ./kata/$1.js
-echo '' >> ./kata/$1.js
-echo '' >> ./kata/$1.js
-echo 'module.exports = ' >> ./kata/$1.js
+echo '// '$1'' >> ./kata/$4.js
+echo '// '$2'' >> ./kata/$4.js
+echo '' >> ./kata/$4.js
+echo '' >> ./kata/$4.js
+echo '' >> ./kata/$4.js
+echo 'module.exports = ' >> ./kata/$4.js
 
 # Create new kata test
 
-echo "const  = require('../$1.js')" >> ./kata/__tests__/$1.test.js
-echo '' >> ./kata/__tests__/$1.test.js
-echo 'describe('$4', () => {' >> ./kata/__tests__/$1.test.js
-echo '	it.each([' >> ./kata/__tests__/$1.test.js
+echo "const  = require('../$4.js')" >> ./kata/__tests__/$4.test.js
+echo '' >> ./kata/__tests__/$4.test.js
+echo 'describe("'$4'", () => {' >> ./kata/__tests__/$4.test.js
+echo '	it.each([' >> ./kata/__tests__/$4.test.js
 
 for (( i=1; i <= $3; i++ ))
 do  
-	echo '		{ input: [], output:  },' >> ./kata/__tests__/$1.test.js
+	echo '		{ input: [], output:  },' >> ./kata/__tests__/$4.test.js
 done
 
 
-echo '	])(' >> ./kata/__tests__/$1.test.js
-echo "		'should return \$output for \$input'," >> ./kata/__tests__/$1.test.js
-echo '		({ input, output }) => {' >> ./kata/__tests__/$1.test.js
-echo '			expect((...input)).toBe(output)' >> ./kata/__tests__/$1.test.js
-echo '		}' >> ./kata/__tests__/$1.test.js
-echo '	)' >> ./kata/__tests__/$1.test.js
-echo '})' >> ./kata/__tests__/$1.test.js
+echo '	])(' >> ./kata/__tests__/$4.test.js
+echo "		'should return \$output for \$input'," >> ./kata/__tests__/$4.test.js
+echo '		({ input, output }) => {' >> ./kata/__tests__/$4.test.js
+echo '			expect((...input)).toBe(output)' >> ./kata/__tests__/$4.test.js
+echo '		}' >> ./kata/__tests__/$4.test.js
+echo '	)' >> ./kata/__tests__/$4.test.js
+echo '})' >> ./kata/__tests__/$4.test.js
